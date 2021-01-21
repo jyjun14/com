448d51f0dc65127d55da1e3ebf7a0bb5fda0e82e
jyjun14 <74234439+jyjun14@users.noreply.github.com>
2021-01-21 21:30:51 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-function button1_click() {
-    alert("버튼 누름 ㅋ");
-    document.getElementById("text").innerHTML = "버튼 누름 ㅋ";
+function check() {
+    var id = document.getElementById("inputid").value;
+    var pwd = document.getElementById("inputpwd").value;
+    if (id == "jyjun14") {
+        if (pwd == "jyjun14!") {
+            alert("올~ㅋ 맞음")
+        }
+        
+        else {
+            alert("틀림")
+        }
+    }
+
+    else {
+        alert("틀림")
+    }
 }
